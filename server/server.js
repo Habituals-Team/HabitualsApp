5a55484401070a6cc2cc503e2dc1c56cd340bf84
andieritter <58995450+andieritter@users.noreply.github.com>
2020-02-23 15:30:00 -0800

    updated server file with two new routes. One gets habit info from db. Other posts user input to db. Added comments

--- server/server.js
+++ server/server.js
@@ -3,13 +3,12 @@ const app = express();
 const path = require("path");
 const bodyParser = require('body-parser');
 // const router = require('./routes/api');
-const habitControllers = require('./controllers/habitControllers')
+const habitControllers = require('./controllers/habitControllers');
+const userControllers = require('./controllers/userControllers');
 
 app.use(bodyParser());
-// const hi = require ("./../client/public/index.html")
 
-// app.use('/api', router);
-
+// route to redner html on home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
@@ -18,8 +17,22 @@ app.get("/dist/bundle.js", (req, res) => {
   res.status(200).sendFile(path.join(__dirname, "../dist/bundle.js"));
 });
 
+// route to render habit options on home page
 app.get('/habits', habitControllers.getHabits, (req, res) => {
   res.status(200).json(res.locals.habits);
 });
+
+// route to post user input from form to db
+app.post('/user-input', userControllers.updateUserHabits, (req, res) => {
+  res.status(200)
+})
+
+// route to get habit information to render when a habit option is clicked
+app.get('/habit-info', habitControllers.getInfo, (req, res) => {
+  res.status(200).json(res.locals.habitInfo);
+})
+
+// route to get a user's chosen habit and routine to render on calendar
+
 
 app.listen(3000);
