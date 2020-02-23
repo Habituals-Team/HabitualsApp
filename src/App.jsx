8b6d0700b2b5354bd0214652ec7d1f6a78fe552b
Aris Razuri <arazuli4@gmail.com>
2020-02-23 15:57:21 -0800

    in progress react routering of boxes container to habitboxescomponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import BoxesContainer from "./BoxesContainer.jsx";
+// this is the destructuring from react router
+import { Switch, Route, Link } from "react-router-dom";
+
 
 // style importing - Eliot test
 import "../public/style.css";
@@ -9,46 +12,26 @@ import "../public/style.css";
 import Button from "@material-ui/core/Button";
 
 // my test components
-import InputForm from "./testComponent - InputForm";
-import InfoForm from "./testComponent - InfoPage";
-import Calendar from "./testComponent - Calendar";
+// import InputForm from "./testComponent - InputForm";
+// import InfoForm from "./testComponent - InfoPage";
+// import Calendar from "./testComponent - Calendar";
 
-// this is the destructuring from react router
-import { Switch, Route, Link } from "react-router-dom";
 
-// Eliot's Test Code
 class App extends Component {
-  // This is rendering using Router - Eliot's test code
+
   render() {
     return (
       <Switch>
         <div>
           <h1>APP</h1>
           {/* <Link to="/">Boxes</Link> */}
-          <Button className="buttonsRouter" component={Link} to="/"></Button>
-          <Route exact strict path="/" component={BoxesContainer} />
+          <Route path="/*" component={BoxesContainer} />
           <div>--------------------- </div>
-          <Button
-            className="buttonsRouter"
-            component={Link}
-            to="/asdf"
-          ></Button>
-          <Route exact strict path="/asdf" component={InfoForm} />
-          <div>--------------------- </div>
-          <Button
-            className="buttonsRouter"
-            component={Link}
-            to="/input"
-          ></Button>
-          <Route exact strict path="/input" component={InputForm} />
-          <div>--------------------- </div>
-          <Button className="buttonsRouter" component={Link} to="/cal"></Button>
-          <Route exact strict path="/cal" component={Calendar} />
         </div>
       </Switch>
     );
   }
 }
-
+//might not be able to do exact or strict path because it might not reach the grandchildren components
 // export default hot(module)(App);
 export default App;
--- src/HabitBoxesComponent.jsx
+++ src/HabitBoxesComponent.jsx
@@ -2,13 +2,11 @@ import React, { Component } from 'react';
 
 //need a parameter to pass down props from BoxesContainer 'props' ex) {props.habitName}
 //need button on click to go to user input form passing in habit._id;
+
 const HabitBoxesComponent = (props) => {
     return (<div>
-        <p>Habit Name: {props.habitName} </p>
-        <p>
-
-        </p>
-
+        <p>{props.habitName}</p>
+        <p>{props.habitInfo}</p>
     </div>)
 }
 
