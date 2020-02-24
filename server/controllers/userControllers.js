18110e5d49dff7128f4c43b83c7f3f9b30f910d0
andieritter <58995450+andieritter@users.noreply.github.com>
2020-02-23 16:28:25 -0800

    userControllers query syntax fixed with template literal

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -4,7 +4,7 @@ const userControllers = {};
 
 // controller to post a user's form info to db
 userControllers.updateUserHabits = (req, res, next) => {
-  db.query('INSERT INTO user_habits (users_id, habits_id, memo, routine_id, start_date, end_date) VALUES (${req.body.usersId}, ${req.body.habitsId}, ${req.body.memo}, ${req.body.routineId}, ${req.body.startDate}, ${req.body.endDate})',
+  db.query(`INSERT INTO user_habits (users_id, habits_id, memo, routine_id, start_date, end_date) VALUES (${req.body.usersId}, ${req.body.habitsId}, ${req.body.memo}, ${req.body.routineId}, ${req.body.startDate}, ${req.body.endDate})`,
     (err, results) => {
       if (err) return next(err);
       req.body.newUserHabit = results.rows;
