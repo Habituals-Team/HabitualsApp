aead8a59d191ffbf3f29f90b73b242dedc180dc4
Joe <jrenolayan@Josephs-MacBook-Pro-2.local>
2020-02-23 21:00:13 -0800

    installed cookie parser, imported client id and client secert

--- server/server.js
+++ server/server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require('body-parser');
+const client_secret = require('../client_secret/client_secret');
 // const router = require('./routes/api');
+const cookieParser = require('cookie-parser');
+
+const client_id = "371087135-djckvfenrkntg92agsc5c7csq2d3cej1.apps.googleusercontent.com";
 
 const api = require('./routes/api');
 
 app.use(bodyParser());
+
+// app.get("/login")
 
 // route to redner html on home page
 app.get("/", (req, res) => {
