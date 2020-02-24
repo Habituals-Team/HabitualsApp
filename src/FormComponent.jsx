fb6cd5150d0c96e25285df924be4ad1ea061f9ff
Admin <admin@Admins-MacBook-Pro.local>
2020-02-24 11:49:06 -0800

    working on form component for input

--- src/FormComponent.jsx
+++ src/FormComponent.jsx
@@ -1,45 +1,40 @@
-import React from "react";
-// import App from "./App.jsx";
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
-/*
-No new node modules installed for this component.
-Component renders a skeleton of form component.
-Payload from this form should posted to psql database.
-Upon click of "Generate Calendar" react calendar should render.
-*/
-
-const Form = () => {
+export default function App() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
+  
   return (
-    <form id="inputForm">
-      <label for="habitMemo">Habit Memo:</label>
-      <input
-        type="text"
-        id="habitMemo"
-        placeholder="Habit Encouragement Memo"
-      />
-      <br></br>
-      <label for="frequency">Frequency: </label>
-      <select id="frequency" name="frequency">
+    <form onSubmit={handleSubmit(onSubmit)}>
+    <div>
+      <input type="number" placeholder="Habit Nickname" name="usersId" ref={register} />
+    </div>
+    <div>
+      <input type="number" placeholder="Habits List" name="habitsId" ref={register({maxLength: 80})} />
+    </div>
+    <div>
+      <input type="text" placeholder="Notes" name="memo" ref={register} />
+    </div>
+    <div></div>
+      <select name="routineId" ref={register({ required: true })}>
         <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
+        <option value=" 2"> 2</option>
+        <option value=" 3"> 3</option>
+        <option value=" 4"> 4</option>
+        <option value=" 5"> 5</option>
+        <option value=" 6"> 6</option>
+        <option value=" 7"> 7</option>
+        <option value=" 8"> 8</option>
+        <option value=" 9"> 9</option>
+        <option value=" 10"> 10</option>
       </select>
-      <label for="frequency">:Per Hour</label>
-      <br></br>
-      <label for="startDate"> Start Date:</label>
-      <input type="date" id="startDate" />
-      <label for="endDate"> End Date:</label>
-      <input type="date" id="endDate" />
-      <input type="submit" value="Generate Calendar" />
+      <input type="text" placeholder="How Often?" name="repetition" ref={register} />
+      <input type="date" placeholder="startDate" name="startDate" ref={register({required: true, maxLength: 11, pattern: /^\S+@\S+$/i})} />
+      <input type="date" placeholder="endDate" name="endDate" ref={register({required: true, maxLength: 12, pattern: /^\S+@\S+$/i})} />
+
+      <input type="submit" />
     </form>
   );
-};
-
-export default Form;
+}
