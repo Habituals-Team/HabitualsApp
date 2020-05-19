dcf79ab346741589715bf84cbf3ba71fcd9da87d
andieritter <58995450+andieritter@users.noreply.github.com>
2020-05-18 21:08:25 -0700

    secured habitController functions  with separated variables and pg queries

--- server/controllers/habitControllers.js
+++ server/controllers/habitControllers.js
@@ -4,7 +4,8 @@ const habitControllers = {};
 
 // middlewear to get habits from habits table
 habitControllers.getHabits = (req, res, next) => {
-  db.query('SELECT * FROM habits', (err, results) => {
+  const text = 'SELECT * FROM habits';
+  db.query(text, (err, results) => {
     if (err) return next(err);
     res.locals.habits = results.rows;
     next();
@@ -13,7 +14,8 @@ habitControllers.getHabits = (req, res, next) => {
 
 // middlewear to get images from images table
 habitControllers.getImages = (req, res, next) => {
-  db.query('SELECT * FROM images', (err, results) => {
+  const text = 'SELECT * FROM images';
+  db.query(text, (err, results) => {
     if (err) return next(err);
     res.locals.images = results.rows;
   })
@@ -22,7 +24,9 @@ habitControllers.getImages = (req, res, next) => {
 //middlewear to get info from habits table
 habitControllers.getInfo = (req, res, next) => {
   const { id } = req.query; //NOTE: added const to make sure it was pulling the id path in the get request
-  db.query(`SELECT * FROM habits WHERE _id = ${id}`,
+  const text = `SELECT * FROM habits WHERE _id = ${id}`;
+  const params = [id];
+  db.query(text, params,
     (err, results) => {
       if (err) return next(err);
       res.locals.habitInfo = results.rows;
